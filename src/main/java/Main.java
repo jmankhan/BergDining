d34e4a34d61150699fc206d00a49fa8b3d519146
jmankhan <jmankhan1@gmail.com>
2015-10-30 12:38:26 -0400

    changed homepage to a very basic layout. will improve somewhat in the future.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,13 +22,21 @@ public class Main {
 
     get("/hello", (req, res) -> "Hello World");
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+//    get("/", (request, response) -> {
+//            Map<String, Object> attributes = new HashMap<>();
+//            attributes.put("message", "Hello World!");
+//
+//            return new ModelAndView(attributes, "index.ftl");
+//        }, new FreeMarkerEngine());
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+    get("/", (req, res) -> {
+      Map<String, Object> attributes = new HashMap<>();
+      attributes.put("message", "Hello World!");
 
+      return new ModelAndView(attributes, "home.ftl");
+
+    }, new FreeMarkerEngine());
+    
     get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
---
+++ src/main/resources/spark/template/freemarker/home.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Berg Dining</title>
+</head>
+
+<body>
+
+<p>Home page for the Berg Dining Server! The github page for this project is available <a href="https://www.github.com/jmankhan/BergDining">here</a> </p>
+<p>If you have any questions, please contact me at jk249997@muhlenberg.edu </p>
+
+</body>
+</html>
