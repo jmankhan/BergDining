143b501c04f106dbcd179da869f33b3c0e7983e3
jmankhan <jmankhan1@gmail.com>
2015-11-08 23:44:51 -0500

    added updater endpoint for testing. Created db with two columns. Created insertion and reading from menu. Insert json menu every week. read most recent json menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
@@ -25,24 +26,28 @@ public class Main {
 	public static void main(String[] args) {
 		port(Integer.valueOf(System.getenv("PORT")));
 		staticFileLocation("/public");
-		
 
 		get("/berg", new Route() {
 			@SuppressWarnings("finally")
 			public Object handle(final Request request, final Response response) {
-				BergParser parser = new BergParser();
+				Connection connection = null;
 				String result = "";
 				try {
-					org.jsoup.Connection.Response resp = parser.getWebpage(parser.url);
-					result = parser.parse(resp.parse(), resp.statusCode());
-					
-				} catch (IOException e) {e.printStackTrace();}
-				finally {
+					connection = DatabaseUrl.extract().getConnection();
+
+					Statement stmt = connection.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT (time, jsonstring) FROM menu ORDER BY time DESC LIMIT 1");
+					rs.next();
+					result = rs.getString(1);
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
 					return result;
 				}
+
 			}
 		});
-		
+
 		get("/", (req, res) -> {
 			Map<String, Object> attributes = new HashMap<>();
 			attributes.put("message", "Hello World!");
@@ -55,18 +60,18 @@ public class Main {
 			Connection connection = null;
 			Map<String, Object> attributes = new HashMap<>();
 			try {
-
-			    connection = DatabaseUrl.extract().getConnection();
+				connection = DatabaseUrl.extract().getConnection();
 
 				Statement stmt = connection.createStatement();
-				
+
 				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS menu (time TIMESTAMP, jsonstring JSON)");
-				stmt.executeUpdate("INSERT INTO menu (time, jsonstring) VALUES (now(), '{'key':'value'}')");
-				ResultSet rs = stmt.executeQuery("SELECT * FROM menu");
+				stmt.executeUpdate("INSERT INTO menu (time, jsonstring) VALUES (now(),'{\"key\":\"value\"}')");
+				ResultSet rs = stmt.executeQuery(
+						"SELECT time FROM menu WHERE age(time) = '7 day' " + "ORDER BY time DESC LIMIT 1");
 
 				ArrayList<String> output = new ArrayList<String>();
 				while (rs.next()) {
-					output.add("Read from DB: " + rs.getTimestamp("TIMESTAMP"));
+					output.add(rs.getString("time"));
 				}
 
 				attributes.put("results", output);
@@ -83,6 +88,26 @@ public class Main {
 			}
 		} , new FreeMarkerEngine());
 
-	}
+		get("/update", new Route() {
+			public Object handle(final Request request, final Response response) {
+				Connection conn = null;
+				try {
+					//add the most recent menu
+					conn = DatabaseUrl.extract().getConnection();
+					Statement stmt = conn.createStatement();
+					
+					BergParser parser = new BergParser();
+					org.jsoup.Connection.Response resp = parser.getWebpage(parser.url);
+					String result = parser.parse(resp.parse(), resp.statusCode());
 
+					// store the parsed the data into the db
+					stmt.executeUpdate("INSERT INTO menu (time, jsonstring) VALUES (now()," + result + ")");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				return "added a new menu to the database";
+			}
+		});
+	}
 }
--- src/main/java/Updater.java
+++ src/main/java/Updater.java
@@ -1,7 +1,38 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Updater {
 
 	public static void main(String args[]) {
-		System.out.println("Update");
+		Connection conn = null;
+
+		try {
+			conn = DatabaseUrl.extract().getConnection();
+			
+			Statement stmt = conn.createStatement();
+			
+			//select if there is a menu from the past week
+			ResultSet rs = stmt.executeQuery("SELECT (time, jsonstring) FROM menu WHERE age(time) <= '7 day' "
+					+ "ORDER BY time DESC LIMIT 1");
+
+			if(!rs.next()) {
+				//initiate parse
+				BergParser parser = new BergParser();
+				org.jsoup.Connection.Response resp = parser.getWebpage(parser.url);
+				String result = parser.parse(resp.parse(), resp.statusCode());
+
+				//store the parsed the data into the db
+				stmt.executeUpdate("INSERT INTO menu (time, jsonstring) VALUES (now(),"+result+")");
+			} else {
+				//the updater has not yet reached one week
+			}
+		} catch(Exception e) {e.printStackTrace(); System.out.println("Error updating");}
 	}
 }
